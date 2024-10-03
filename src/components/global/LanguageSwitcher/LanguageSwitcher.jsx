0f38460123812a9032ca662aa07e36be579a1818
Quentin  VERDIER <quent1.verdier@gmail.com>
2024-10-03 15:27:07 +0200

    🔄 Change components file's type

--- src/components/global/LanguageSwitcher/LanguageSwitcher.jsx
+++ src/components/global/LanguageSwitcher/LanguageSwitcher.jsx
@@ -50,7 +50,7 @@ export default function LanguageSwitcher({ className = '' }) {
       </div>
 
       {isOpen && (
-        <div className="absolute right-0 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5">
+        <div className="absolute right-0 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black/5">
           <div className="py-1" role="menu" aria-orientation="vertical" aria-labelledby="options-menu">
             {languages.map((lang) => (
               <button
