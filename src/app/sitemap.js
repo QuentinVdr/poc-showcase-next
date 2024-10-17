3ebf92d11ff6a4052cf0081d16280ed5633d3a57
Quentin  VERDIER <quent1.verdier@gmail.com>
2024-10-17 10:31:25 +0200

    ✨ Add sitemap generation for dynamic blog posts and static pages

---
+++ src/app/sitemap.js
@@ -0,0 +1,45 @@
+import { availableLanguages } from '@/i18n/availableLanguages';
+import { getDocuments } from 'outstatic/server';
+
+export default async function sitemap() {
+  // Set your website's URL
+  const baseUrl = 'https://localhost:3000'; // TODO : Change this URL
+
+  // Fetch all posts
+  const postEntries = availableLanguages
+    .map((locale) => {
+      const posts = getDocuments(`${locale}-posts`, ['slug', 'publishedAt']);
+      return posts.map((post) => ({
+        url: `${baseUrl}/blog/${post.slug}`,
+        lastModified: new Date(post.publishedAt),
+        changeFrequency: 'weekly',
+        priority: 0.7,
+      }));
+    })
+    .flat();
+
+  // Define static pages
+  const staticPages = [
+    {
+      url: baseUrl,
+      lastModified: new Date(),
+      changeFrequency: 'daily',
+      priority: 1,
+    },
+    {
+      url: `${baseUrl}/about`,
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.8,
+    },
+    {
+      url: `${baseUrl}/blog`,
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.5,
+    },
+  ];
+
+  // Combine and return all entries
+  return [...staticPages, ...postEntries];
+}
