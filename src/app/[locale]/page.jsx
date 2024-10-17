7af9bb88de6a606ecf37b8c0e5562e1702f3dfd9
Quentin  VERDIER <quent1.verdier@gmail.com>
2024-10-17 15:13:03 +0200

    ✨ Update HomePage layout: restructure content for improved semantics and readability; enhance heading styles in global CSS

--- src/app/[locale]/page.jsx
+++ src/app/[locale]/page.jsx
@@ -8,14 +8,24 @@ export default function HomePage() {
   return (
     <main className="mx-auto my-4 flex w-10/12 max-w-6xl flex-col gap-4">
       <h1 className="text-4xl">{t('title')}</h1>
-      <p>{t('description')}</p>
-      <div className="grid grid-cols-1 gap-4 md:grid-cols-2">
-        {blocks.map((block) => (
-          <div key={block.title}>
-            <h2>{block.title}</h2>
-            <p>{block.description}</p>
-          </div>
-        ))}
+      <div>
+        <p>{t('description')}</p>
+        <h1>h1</h1>
+        <h2>h2</h2>
+        <h3>h3</h3>
+        <h4>h4</h4>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat harum dolore repellendus eius in possimus
+          veniam minima id soluta numquam rerum consectetur, officiis, minus repudiandae?{' '}
+        </p>
+        <div className="grid grid-cols-1 gap-4 md:grid-cols-2">
+          {blocks.map((block) => (
+            <div key={block.title}>
+              <h2>{block.title}</h2>
+              <p>{block.description}</p>
+            </div>
+          ))}
+        </div>
       </div>
     </main>
   );
