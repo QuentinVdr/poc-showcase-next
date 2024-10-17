411a0784272695df94bea3cc8aa711c08f560558
Quentin  VERDIER <quent1.verdier@gmail.com>
2024-10-17 14:24:53 +0200

    ✨ Refactor sitemap generation: extract post mapping logic into a separate function for improved readability

--- src/app/sitemap.js
+++ src/app/sitemap.js
@@ -3,18 +3,19 @@ import { DOMAIN } from '@utils/Constant';
 import { getDocuments } from 'outstatic/server';
 
 export default async function sitemap() {
-  // Fetch all posts
-  const postEntries = availableLanguages
-    .map((locale) => {
-      const posts = getDocuments(`${locale}-posts`, ['slug', 'publishedAt']);
-      return posts.map((post) => ({
-        url: `${DOMAIN}/blog/${post.slug}`,
-        lastModified: new Date(post.publishedAt),
-        changeFrequency: 'weekly',
-        priority: 0.7,
-      }));
-    })
-    .flat();
+  // Define a function to map a post to a sitemap entry
+  const mapPostToSitemapEntry = (post) => ({
+    url: `${DOMAIN}/blog/${post.slug}`,
+    lastModified: new Date(post.publishedAt),
+    changeFrequency: 'weekly',
+    priority: 0.7,
+  });
+
+  // Fetch all posts and map to sitemap entries
+  const postEntries = availableLanguages.flatMap((locale) => {
+    const posts = getDocuments(`${locale}-posts`, ['slug', 'publishedAt']);
+    return posts.map(mapPostToSitemapEntry);
+  });
 
   // Define static pages
   const staticPages = [
